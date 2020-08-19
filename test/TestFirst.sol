312a163086ba326404ed5b050c1c2ee77a9d4613
Muhammad Anas Baig <anasbaigmughal@gmail.com>
2020-08-19 13:15:27 +0500

    Update TestFirst.sol

--- test/TestFirst.sol
+++ test/TestFirst.sol
@@ -8,6 +8,8 @@ contract TestFirst {
 
     function testAge() public{
         First meta = First(DeployedAddresses.First());
+        // int a = meta.age; // not accessible as internal variable
+        // int a = meta.age1 // even public variables are not accessible as a variable directly as in the mentioned statement, but we could access it as a function like int a = age1()
         Assert.equal(meta.getAge(),40,"Age is not equal to 40");
     }
 }
